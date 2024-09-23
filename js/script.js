69da47aa3d0c12a50982b1b3ed3688c480303968
Ilja <ilja.solomatin@mail.ru>
2024-09-23 15:41:52 +0100

    Added ccs for fixing flexbox gap
    - In case flexbox gap has issues in some browsers/version

--- js/script.js
+++ js/script.js
@@ -52,7 +52,6 @@ const sectionHeroEl = document.querySelector(".section-hero");
 const obs = new IntersectionObserver(
   function (entries) {
     const ent = entries[0];
-    console.log(ent);
 
     if (!ent.isIntersecting) {
       document.body.classList.add("sticky");
